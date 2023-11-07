a9845fac86a1775e4c48503d2b60ca967e78364b
unknown <jellarave@gmail.com>
2023-11-07 09:33:18 -0500

    Se realiza una nueva publicacion de la base de datos ya que la anterior se exporto de forma incompleta

--- db_inncloud.sql
+++ db_inncloud.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 06-11-2023 a las 23:26:18
+-- Tiempo de generación: 07-11-2023 a las 15:24:43
 -- Versión del servidor: 10.4.28-MariaDB
 -- Versión de PHP: 8.0.28
 
@@ -20,6 +20,156 @@ SET time_zone = "+00:00";
 --
 -- Base de datos: `db_inncloud`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `documents`
+--
+
+CREATE TABLE `documents` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `doc_name` varchar(60) NOT NULL,
+  `doc_prefix` varchar(20) NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Volcado de datos para la tabla `documents`
+--
+
+INSERT INTO `documents` (`id`, `doc_name`, `doc_prefix`, `created_at`, `updated_at`) VALUES
+(1, 'Instructivo', 'INS', '2023-11-05 19:09:13', '2023-11-05 19:26:43'),
+(2, 'Capacitacion', 'CPC', '2023-11-06 15:59:39', '2023-11-06 15:59:39'),
+(3, 'Aprendizaje', 'APD', '2023-11-06 20:33:02', '2023-11-06 20:33:02'),
+(4, 'Inducción', 'IND', '2023-11-06 22:33:54', '2023-11-06 22:33:54'),
+(5, 'Formación', 'FMO', '2023-11-06 22:34:40', '2023-11-06 22:34:40');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `failed_jobs`
+--
+
+CREATE TABLE `failed_jobs` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `uuid` varchar(255) NOT NULL,
+  `connection` text NOT NULL,
+  `queue` text NOT NULL,
+  `payload` longtext NOT NULL,
+  `exception` longtext NOT NULL,
+  `failed_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `migrations`
+--
+
+CREATE TABLE `migrations` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `migration` varchar(255) NOT NULL,
+  `batch` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Volcado de datos para la tabla `migrations`
+--
+
+INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
+(35, '2014_10_12_000000_create_users_table', 1),
+(36, '2014_10_12_100000_create_password_resets_table', 1),
+(37, '2019_08_19_000000_create_failed_jobs_table', 1),
+(38, '2019_12_14_000001_create_personal_access_tokens_table', 1),
+(39, '2023_11_03_193817_create_processes_table', 1),
+(40, '2023_11_03_193851_create_documents_table', 1),
+(41, '2023_11_03_230831_create_process_documents_table', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `password_resets`
+--
+
+CREATE TABLE `password_resets` (
+  `email` varchar(255) NOT NULL,
+  `token` varchar(255) NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `personal_access_tokens`
+--
+
+CREATE TABLE `personal_access_tokens` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `tokenable_type` varchar(255) NOT NULL,
+  `tokenable_id` bigint(20) UNSIGNED NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `token` varchar(64) NOT NULL,
+  `abilities` text DEFAULT NULL,
+  `last_used_at` timestamp NULL DEFAULT NULL,
+  `expires_at` timestamp NULL DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `processes`
+--
+
+CREATE TABLE `processes` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `pro_prefix` varchar(20) NOT NULL,
+  `pro_name` varchar(60) NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Volcado de datos para la tabla `processes`
+--
+
+INSERT INTO `processes` (`id`, `pro_prefix`, `pro_name`, `created_at`, `updated_at`) VALUES
+(1, 'ING', 'Ingenieria', '2023-11-04 21:38:56', '2023-11-05 19:25:08'),
+(2, 'DTD', 'Doctorado', '2023-11-05 03:54:11', '2023-11-05 03:54:34'),
+(3, 'TEC', 'Tecnologo', '2023-11-05 19:07:22', '2023-11-05 19:07:22'),
+(4, 'TEC', 'Tecnico', '2023-11-06 22:35:08', '2023-11-06 22:35:08'),
+(5, 'ETD', 'Estudiante', '2023-11-06 22:35:30', '2023-11-06 22:35:30');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `process_documents`
+--
+
+CREATE TABLE `process_documents` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `doc_name` varchar(50) NOT NULL,
+  `doc_code` int(11) NOT NULL,
+  `doc_content` varchar(4000) NOT NULL,
+  `id_process` bigint(20) UNSIGNED NOT NULL,
+  `id_document` bigint(20) UNSIGNED NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Volcado de datos para la tabla `process_documents`
+--
+
+INSERT INTO `process_documents` (`id`, `doc_name`, `doc_code`, `doc_content`, `id_process`, `id_document`, `created_at`, `updated_at`) VALUES
+(3, 'Aprendizaje', 2, 'Cursos de aprendices', 2, 2, '2023-11-06 18:22:28', '2023-11-06 18:22:28'),
+(6, 'Ingenieria', 5, 'Capoacitacion de ingenieros', 3, 2, '2023-11-06 20:49:54', '2023-11-06 20:49:54'),
+(7, 'Arquitectura', 3, 'Clases de arquitectura', 1, 2, '2023-11-06 20:52:05', '2023-11-06 20:52:05'),
+(14, 'Introduccion', 1, 'Introduccion', 3, 2, '2023-11-07 00:23:01', '2023-11-07 00:23:01'),
+(15, 'Ingenieria', 6, 'Clases de ingenieria', 1, 2, '2023-11-07 05:01:15', '2023-11-07 05:01:15');
 
 -- --------------------------------------------------------
 
@@ -50,6 +200,53 @@ INSERT INTO `users` (`id`, `name`, `email`, `email_verified_at`, `password`, `re
 --
 
 --
+-- Indices de la tabla `documents`
+--
+ALTER TABLE `documents`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `failed_jobs`
+--
+ALTER TABLE `failed_jobs`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `failed_jobs_uuid_unique` (`uuid`);
+
+--
+-- Indices de la tabla `migrations`
+--
+ALTER TABLE `migrations`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `password_resets`
+--
+ALTER TABLE `password_resets`
+  ADD PRIMARY KEY (`email`);
+
+--
+-- Indices de la tabla `personal_access_tokens`
+--
+ALTER TABLE `personal_access_tokens`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `personal_access_tokens_token_unique` (`token`),
+  ADD KEY `personal_access_tokens_tokenable_type_tokenable_id_index` (`tokenable_type`,`tokenable_id`);
+
+--
+-- Indices de la tabla `processes`
+--
+ALTER TABLE `processes`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `process_documents`
+--
+ALTER TABLE `process_documents`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `process_documents_id_process_foreign` (`id_process`),
+  ADD KEY `process_documents_id_document_foreign` (`id_document`);
+
+--
 -- Indices de la tabla `users`
 --
 ALTER TABLE `users`
@@ -61,10 +258,57 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT de la tabla `documents`
+--
+ALTER TABLE `documents`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT de la tabla `failed_jobs`
+--
+ALTER TABLE `failed_jobs`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `migrations`
+--
+ALTER TABLE `migrations`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=42;
+
+--
+-- AUTO_INCREMENT de la tabla `personal_access_tokens`
+--
+ALTER TABLE `personal_access_tokens`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
+
+--
+-- AUTO_INCREMENT de la tabla `processes`
+--
+ALTER TABLE `processes`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT de la tabla `process_documents`
+--
+ALTER TABLE `process_documents`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+
+--
 -- AUTO_INCREMENT de la tabla `users`
 --
 ALTER TABLE `users`
   MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `process_documents`
+--
+ALTER TABLE `process_documents`
+  ADD CONSTRAINT `process_documents_id_document_foreign` FOREIGN KEY (`id_document`) REFERENCES `documents` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `process_documents_id_process_foreign` FOREIGN KEY (`id_process`) REFERENCES `processes` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
